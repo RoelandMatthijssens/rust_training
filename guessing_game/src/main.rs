9ef624e418bd0ab874e32ccbe4c828b68cd87075
Roeland <roeland.matthijssens@vikingco.com>
2017-11-19 18:50:16 +0100

    Added loop and error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,18 +6,29 @@ use rand::Rng;
 
 fn main() {
     let secret = rand::thread_rng().gen_range(0, 101);
-    println!("The secret to be guessed is {}", secret);
-    println!("Make a guess!");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse()
-        .expect("Please enter a number");
-    println!("You guessed {}", guess);
 
-    match guess.cmp(&secret){
-        Ordering::Less    => println!("Too small"),
-        Ordering::Greater => println!("Too large"),
-        Ordering::Equal   => println!("Finished!"),
+    loop {
+        println!("Make a guess!");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 =match guess.trim().parse() {
+            Ok(x)   => x,
+            Err(_)  => {
+                println!("Please enter a number");
+                continue;
+            },
+        };
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret){
+            Ordering::Less    => println!("Too small"),
+            Ordering::Greater => println!("Too large"),
+            Ordering::Equal   => {
+                println!("Finished!");
+                break;
+            }
+        };
+        println!();
     }
 }
