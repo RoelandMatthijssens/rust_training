0752a62e9327ac5f9f0969743754ea8704e3b6df
Roeland <roeland.matthijssens@vikingco.com>
2017-11-19 18:38:42 +0100

    Generate random number at start of game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,11 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
+    let secret = rand::thread_rng().gen_range(0, 101);
+    println!("The secret to be guessed is {}", secret);
     println!("Make a guess!");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
